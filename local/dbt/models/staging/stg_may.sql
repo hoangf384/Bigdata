80aa888cd0f34e6ddda62be4b2c1792d47336a7d
hoangf384 <126351927+hoangf384@users.noreply.github.com>
2026-03-09 12:13:35 +0700

    dbt: update constraint
    force user_id to varchar

--- local/dbt/models/staging/stg_may.sql
+++ local/dbt/models/staging/stg_may.sql
@@ -1,6 +1,7 @@
 SELECT
-    CAST(user_id AS VARCHAR) AS user_id,
+    NULLIF(TRIM(CAST(user_id AS VARCHAR)), 'NULL') AS user_id,
     contentName AS content_name,
     screenName AS screen_name,
-    CAST(TotalView AS BIGINT) AS total_view,
+    CAST(TotalView AS BIGINT) AS total_view
 FROM {{ source('raw', 'thang5') }}
+WHERE user_id IS NOT NULL
